40eeb94f283974c977c4a1c49bf4b2455742a000
Jinliang Liu <liujinliang.627@gmail.com>
2025-01-22 12:19:17 -0800

    New file

---
+++ boba.py
@@ -0,0 +1,18 @@
+import itertools
+import math
+# def main():
+#     print(math.factorial(4) / math.factorial(2))
+    
+#     leters = ['b', 'o', 'b', 'a']
+#     perms = set(itertools.permutations(leters))
+#     for perm in perms:
+#         permStr = "".join(perm)
+#         print(permStr)
+
+import math
+def main():
+    n = math.factorial(4)
+    d = math.factorial(2)
+    print(n / d)
+
+main()
